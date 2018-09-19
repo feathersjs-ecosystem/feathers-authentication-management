fc2505b73be642e0e8db1d76abce9875117be84a
Layton Whiteley <lwhiteley@medullan.com>
2018-09-18 23:22:35 -0500

    make delta for time tests 1700

--- test/sendResetPwd.test.js
+++ test/sendResetPwd.test.js
@@ -271,7 +271,7 @@ function makeDateTime(options1) {
 }
 
 function aboutEqualDateTime(time1, time2, msg, delta) {
-  delta = delta || 1500;
+  delta = delta || 1700;
   const diff = Math.abs(time1 - time2);
   assert.isAtMost(diff, delta, msg || `times differ by ${diff}ms`);
 }
