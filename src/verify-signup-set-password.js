e44b7dedde13f959c06061958b9dbb839d32c584
Gautier <6089653+GautierT@users.noreply.github.com>
2021-06-28 13:04:10 +0200

    Update verify-signup-set-password.js
    Remove verifyProps to reduce risk of brute-force the short token.

--- src/verify-signup-set-password.js
+++ src/verify-signup-set-password.js
@@ -69,8 +69,7 @@ async function verifySignupSetPassword (options, query, tokens, password, field,
   ]);
 
   if (!Object.keys(tokens).every((key) => tokens[key] === user1[key])) {
-    // Commented to fix a security issue where password is set even if token don't match.
-    // await eraseVerifyPropsSetPassword(user1, user1.isVerified, {}, password, field);
+    await eraseVerifyProps(user1, user1.isVerified, {});
 
     throw new errors.BadRequest(
       'Invalid token. Get for a new one. (authLocalMgnt)',
@@ -89,6 +88,19 @@ async function verifySignupSetPassword (options, query, tokens, password, field,
   const user3 = await notifier(options.notifier, 'verifySignupSetPassword', user2, notifierOptions);
   return options.sanitizeUserForClient(user3);
 
+  async function eraseVerifyProps (user, isVerified, verifyChanges) {
+    const patchToUser = Object.assign({}, verifyChanges || {}, {
+      isVerified,
+      verifyToken: null,
+      verifyShortToken: null,
+      verifyExpires: null,
+      verifyChanges: {}
+    });
+
+    const result = await usersService.patch(user[usersServiceIdName], patchToUser, {});
+    return result;
+  }
+  
   async function eraseVerifyPropsSetPassword (user, isVerified, verifyChanges, password, field) {
     const hashedPassword = await hashPassword(options.app, password, field);
 
